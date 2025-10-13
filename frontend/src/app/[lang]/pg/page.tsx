e737b459db75b2b9b1febda443b8cd9dc67df07d
Abdel117 <awmahfoud@proton.me>
2025-10-13 13:09:31 +0200

    Actualizado compose.yaml

--- frontend/src/app/[lang]/pg/page.tsx
+++ frontend/src/app/[lang]/pg/page.tsx
@@ -1,9 +1,29 @@
 import PixelatedImage from "@/components/motion/PixelatedImage";
 
+
+
+type CreateElement = {
+    (tag: 'a'): HTMLAnchorElement
+    (tag: 'canvas'): HTMLCanvasElement
+}
+
+const createElement = ((tag) => {
+    switch (tag) {
+        case 'a':
+            return new HTMLAnchorElement();
+        case 'canvas':
+        default:
+            throw new Error(`Unknown tag "${tag}"`);
+    }
+}) as CreateElement;
+
+createElement('a')
+createElement('canvas')
+
 export default function Pg(){
     return (
         <div className={"flex flex-col items-center justify-center w-screen h-screen"}>
-            <PixelatedImage pixelSize={40} rows={15} cols={10} src={"/assets/covers/1.webp"}/>
+            <PixelatedImage pixelSize={40} rows={11} cols={10} src={"/assets/covers/1.webp"}/>
         </div>
     )
 }
